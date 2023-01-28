3397ac1277b483102e73d4217efc3eb00fb2be00
Данил Казанский <danii.kazanskiy@mail.ru>
2023-01-28 14:58:01 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string anum; //сгенерированное число
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btnAutorization_Click(object sender, RoutedEventArgs e)
+        {
+            NumWindow NW = new NumWindow();
+            
+            string login = "Иван"; //задаем логин и пароль
+            string password = "1234";
+
+            if (login == tbLogin.Text && password== tbPassword.Text) //условие на проверку введенных значений
+            {
+                Random rnd = new Random();
+                int num = rnd.Next(10000, 99999); // случайное 5-ое число
+
+                MessageBox.Show(num.ToString(), "Запомните одноразовый код", MessageBoxButton.OK); //вывод сообщения со сгенерированным значением
+                anum = num.ToString();
+
+                this.Hide();
+                NW.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка ввода", MessageBoxButton.OK); //ошибка при вводе
+            }
+        }
+
+
+        public void successInp() //успешный вход
+        {
+            MessageBox.Show("Вы авторизировались", "Поздравляем!", MessageBoxButton.OK);
+        }
+
+        public void unsuccessInp() //при входе было допущена ошибка
+        {
+            MessageBox.Show("Введено неверное значение ожидайте 60 секунд", "Ошибка ввода", MessageBoxButton.OK);
+        }
+    }
+}
---
+++ WpfApp3/NumWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для NumWindow.xaml
+    /// </summary>
+    public partial class NumWindow : Window
+    {
+        public NumWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MN = new MainWindow();
+
+            if (tbNum.Text == MN.anum) //сравниваем введенное число с фактическим
+            {
+                
+                MN.Show();
+                this.Close();
+                MN.successInp(); //успешная авторизация
+            }
+            else
+            {
+                MN.Show();
+                this.Close();
+                MN.unsuccessInp(); //была допущена ошибка
+            }
+
+        }
+    }
+}
